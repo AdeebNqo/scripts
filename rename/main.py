c4abe150f896e4c2c09f2fedd2691f1bcffdc482
AdeebNqo <adeebnqo@gmail.com>
2013-10-31 15:00:14 +0200

    abt to write random name generator

--- rename/main.py
+++ rename/main.py
@@ -1,4 +1,11 @@
 import sys
+from string import ascii_lowercase
+
+lc = list(ascii_lowercase)
+letters = [0,1,2,3,4,5,6,7,8,9]+lc
+
+def getname():
+	print('name given')
 try:
 	path = sys.argv[1]
 	print(path)
