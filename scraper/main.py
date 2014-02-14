68891fe9e8897c1631a0c6e0af2d2ef4f81ef7f7
AdeebNqo <adeebnqo@gmail.com>
2014-02-14 19:19:55 +0200

    fixed cntlm proxy, can now retrieve pages

--- scraper/main.py
+++ scraper/main.py
@@ -3,7 +3,12 @@
 # Python script for scraping data from property24.com
 #
 #
+import urllib2
 def main():
 	print('\t-Property24 address and price scraper--')
+	page = get_page("http://www.property24.com/for-sale/cape-town/western-cape/432")
+	print(page)
+def get_page(link):
+	return urllib2.urlopen(link).read()
 if __name__=="__main__":
 	main()
