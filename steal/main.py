319aafcf754de69cf519743f018330e9f9b74a72
AdeebNqo <adeebnqo@gmail.com>
2014-02-22 17:02:33 +0200

    pynotify failing me. Discontinued

--- steal/main.py
+++ steal/main.py
@@ -2,32 +2,53 @@ import sys
 import subprocess
 import time
 import pyinotify
-from pyinotify import *
+import asyncore
 
-def run_cmd(cmd):
-	return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-storage_dir = sys.argv[1]
-#print("Will wait for about 20 seconds in case first video hasn't started.")
-#time.sleep(20)
 
-sys.stdout.write('Searching for lynda.com widget pid...')
-#Getting process number of the flash player
-result = run_cmd('lsof -n | grep Flash')
-output = result.stdout.read().strip()
-tokens = output.split()
-lynda_pid = int(tokens[1])
-sys.stdout.write('Done!\n')
+
 #------------------------------------------------------
 #Monitor change to folder which belongs to this process
 #------------------------------------------------------
 
 print('Now setting up folder listener...')
 
-class PClose(ProcessEvent):
-        def process_IN_DELETE(self, event):
-            print('deleted')
+wm = pyinotify.WatchManager()
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE
 
-watchMonitor = pyinotify.WatchManager()
-notifier = pyinotify.Notifier(watchMonitor)
-watchMonitor.add_watch('/proc/'+str(lynda_pid)+'/fd', pyinotify.ALL_EVENTS)
-notifier.loop()
+class EventHandler(pyinotify.ProcessEvent):
+	def process_IN_CREATE(self,event):
+		print('created')
+	def process_IN_DELETE(self, event):
+		print('deleted')
+	def process_default(self,event):
+		print('something happed in folder')
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch('/tmp', mask, rec=True)
+#wdd = wmanager.add_watch('/proc/'+str(lynda_pid)+'/fd/', actions, rec=True)
+#print('watching /proc/'+str(lynda_pid)+'/fd/')
+asyncore.loop()
+
+#------------------------------------------------------
+
+#------------------------------------------------------
+def run_cmd(cmd):
+	return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+
+storage_dir = sys.argv[1]
+
+sys.stdout.write('Searching for lynda.com widget pid...')
+#Getting process number of the flash player
+result = run_cmd('lsof -n | grep Flash')
+output = result.stdout.read().strip()
+tokens = output.split()
+if (len(tokens)>0):
+	lynda_pid = int(tokens[1])
+	sys.stdout.write('Done!\n')
+	while(True):
+		a =1
+else:
+	print('lynda not detected.')
+
